7458b6a0fc1f2de535f6e902cce5207111482873
DONGCHI LI <dl1321@ic.ac.uk>
2022-01-27 09:26:00 +0000

    add the new query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -13,7 +13,7 @@ public class QueryProcessor {
             return "Imperial College is a university in London";
         }
 
-        if (query.toLowerCase().contains("your team")) {
+        if (query.toLowerCase().contains("your name")) {
             return "GoTeam";
         }
 
